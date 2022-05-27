8ec705ff4a7638b00bd9ff4c0997da5b566e1d16
jose.raul.navarro.avila27@uabc.edu.mx <jose.raul.navarro.avila27@uabc.edu.mx>
2022-05-26 23:20:22 -0700

    Agregar archivos de proyecto.

---
+++ While1.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+                Console.ReadKey();
+            }
+            while (i <= 5);
+
+        }
+
+    }
+}
